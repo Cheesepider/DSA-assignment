ff38df22207c80b61691719ca7c730935f3983c4
Cheesepider <jlohziyang2@gmail.com>
2026-07-23 17:00:56 +0800

    entity equal() updated.

--- Hotel/src/entity/Room.java
+++ Hotel/src/entity/Room.java
@@ -46,6 +46,20 @@ public class Room {
     public void setRoomStatus(String roomStatus) {
         this.roomStatus = roomStatus;
     }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Room other = (Room) obj;
+        return roomID.equals(other.roomID);
+    }
 
     @Override
     public String toString() {
